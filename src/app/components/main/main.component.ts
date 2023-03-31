c198e428ed245486af1991f0d07eec8ef351b313
James <jamespatterson.feswe@yahoo.com>
2023-03-31 17:21:39 -0400

    chore: start initial build for products

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpService } from '../../services/http/http.service';
 import { Products } from '../../models/products.interface';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -11,15 +11,23 @@ import { Subscription } from 'rxjs';
 export class MainComponent implements OnInit, OnDestroy {
 
   private productsSubscription!: Subscription;
+  protected products$: BehaviorSubject<Products[]>;
 
-  constructor(private http: HttpService) {}
+  constructor(private http: HttpService) {
+    this.products$ = new BehaviorSubject<Products[]>([])
+  }
 
   ngOnInit(): void {
-    this.productsSubscription = this.http.getData('https://fakestoreapi.com/products').subscribe({
-      next: (data: Products[]) => {
-        console.log(data);
-      }
-    })
+    this.productsSubscription =
+      this.http.getRequest('https://fakestoreapi.com/products').subscribe({
+        next: (data: Products[]) => {
+          console.log(data);
+          this.products$.next(data);
+        },
+        error: (err: unknown) => {
+          console.error(err);
+        }
+      });
   }
 
   ngOnDestroy(): void {
--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Products } from '../../models/products.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  public getData(url: string, options?: any): Observable<any> {
+  public getRequest(url: string, options?: any): Observable<Products[]> {
     return this.http.get<any>(`https://fakestoreapi.com/products`);
   }
 }
