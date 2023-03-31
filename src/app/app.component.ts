e5641fe5b5f00bbab1c04b2376b253dcc69fb5a4
James <jamespatterson.feswe@yahoo.com>
2023-03-31 14:16:04 -0400

    chore: add code to header/footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-main></app-main>`,
+  template: `
+    <app-header [title]="title"></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>
+  `,
 })
 export class AppComponent {
   title: string = 'Faux Store';
